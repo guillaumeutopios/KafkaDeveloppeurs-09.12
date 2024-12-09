0891f60fa59094010e684e6ed84fb199f5645f86
Guillaume Mairesse <guillaume@utopios.solutions>
2024-12-09 15:38:24 +0100

    exo1 + demo 2 partitioner

---
+++ 01 - Bases/1.demo-base-kafka/src/main/java/com/example/demobasekafka/configuration/KafkaProducerConfig.java
@@ -0,0 +1,51 @@
+//package com.example.demobasekafka.configuration;
+//
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.apache.kafka.common.serialization.StringSerializer;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.kafka.core.ProducerFactory;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class KafkaProducerConfig {
+//    @Value("${spring.kafka.bootstrap-servers}")
+//    private String bootstrapServers;
+//
+//    @Value("${critical.partition}")
+//    private String criticalPartition;
+//
+//    private Map<String, Object> commonConfig() {
+//        Map<String, Object> configProps = new HashMap<>();
+//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+//        configProps.put("critical.partition", criticalPartition);
+//        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        configProps.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, MessagePartitioner.class);
+//        return configProps;
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, String> kafkaTemplateSync() {
+//        Map<String, Object> configProps = commonConfig();
+//        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+//        ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(configProps);
+//        return new KafkaTemplate<>(producerFactory);
+//    }
+//
+//    @Bean
+//    @Primary
+//    public KafkaTemplate<String, String> kafkaTemplateAsync() {
+//        Map<String, Object> configProps = commonConfig();
+//        configProps.put(ProducerConfig.ACKS_CONFIG, "1");
+//        ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(configProps);
+//        return new KafkaTemplate<>(producerFactory);
+//    }
+//
+//}
---
+++ 01 - Bases/1.demo-base-kafka/src/main/java/com/example/demobasekafka/service/NotificationProducerService.java
@@ -0,0 +1,44 @@
+package com.example.demobasekafka.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class NotificationProducerService {
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+//
+//    public NotificationProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
+
+
+    private final KafkaTemplate<String, String> kafkaTemplateAsync;
+
+    private final KafkaTemplate<String, String> kafkaTemplateSync;
+
+    public NotificationProducerService(/*@Qualifier("kafkaTemplateAsync")*/ KafkaTemplate<String, String> kafkaTemplateAsync,/*@Qualifier("kafkaTemplateSync")*/ KafkaTemplate<String, String> kafkaTemplateSync) {
+        this.kafkaTemplateAsync = kafkaTemplateAsync;
+        this.kafkaTemplateSync = kafkaTemplateSync;
+    }
+
+    public void sendCriticalNotification(String topic, String message) {
+        try {
+            kafkaTemplateSync.send(topic,"new_message", message).get();
+        }catch (InterruptedException | ExecutionException ex) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Failed to send message", ex);
+        }
+    }
+
+    public void sendGeneralNotification(String topic, String message) {
+        kafkaTemplateAsync.send(topic,"general_message",message).whenCompleteAsync((result, ex) -> {
+           if(ex != null) {
+               System.out.println("Message sent successfully");
+           }else {
+               System.out.println("Failed to send message " +ex.getMessage());
+           }
+        });
+    }
+}
